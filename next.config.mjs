bf14f1c7d3d5c9a0e820e870b869d502b2e94d00
Julien Texier <julien.texier@taitounited.fi>
2024-09-28 17:13:50 +0300

    Adding project Helsinki Casting

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,24 @@
 import mdx from '@next/mdx';
 
 const withMDX = mdx({
-    extension: /\.mdx?$/,
-    options: { },
+  extension: /\.mdx?$/,
+  options: {},
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  images: {
+    remotePatterns: [
+      // Github Avatars
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/**',
+      },
+    ],
+  },
 };
 
 export default withMDX(nextConfig);
